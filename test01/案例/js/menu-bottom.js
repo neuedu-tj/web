7456d0f39de0e8b6f84fe34e816fc21b11b88a35
Administrator <Administrator@TECH-100>
2017-07-05 11:17:08 +0800

    增加了切换效果

--- test01/案例/js/menu-bottom.js
+++ test01/案例/js/menu-bottom.js
@@ -1,10 +1,36 @@
 $(function(){
 	
-	var orign ; 
-	
+
+	/* 控制切换 */	
 	$(".menu-div").click(function(){
 		
-		console.log(orign)
+		/* 将菜单图标都改为 normal */
+		$(".menu-div").each(function(){
+			$(this).find("img").attr("src" , $(this).data("icon-normal"));
+		})
+		
+		/* 再将被点击图标改为 press */
+		var iocnPress = $(this).data("icon-press")
+		$(this).find("img").attr("src" , iocnPress)
+		
+		/* 获取 中间部分的层 */
+		
+		
+		var target = $(this).data("target")
+		$("#wrap-chat , #wrap-add-friend , #wrap-discovery , #wrap-me").each(function(){
+			
+			if( $(this)[0].id==target){
+				
+				$(this).show(200);
+				
+			}else {
+				$(this).hide();
+			}
+		
+		})
+		
+		
+		
 	});
 	
 })
