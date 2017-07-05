4a503598906eb3050555cbaedde547904813d27b
Administrator <Administrator@TECH-100>
2017-07-05 09:29:36 +0800

    增加了弹出层的 脚本 ( jquery ) 控制

---
+++ test01/案例/js/menu-header-popup.js
@@ -0,0 +1,17 @@
+$(function(){
+	
+	
+	
+//	/*  控制顶层弹出菜单层 : start  */
+//	/* 弹出层  和 弹出小三角   初始应该为 隐藏  */
+	$("#popup-menu , #small-triangle").hide();
+
+	$('#add').click(function(){
+		$("#popup-menu,#small-triangle").toggle(200);
+	})
+	/*  控制顶层弹出菜单层 : end  */
+	
+	
+	
+	
+})
