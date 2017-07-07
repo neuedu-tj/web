c6e175b0b02d7fc6b8b85a06d3c18b2b5c708dcf
Administrator <Administrator@TECH-100>
2017-07-07 11:32:00 +0800

    修改了  首页小三角的写法

--- test01/案例/js/menu-header-popup.js
+++ test01/案例/js/menu-header-popup.js
@@ -4,10 +4,11 @@ $(function(){
 	
 //	/*  控制顶层弹出菜单层 : start  */
 //	/* 弹出层  和 弹出小三角   初始应该为 隐藏  */
-	$("#popup-menu , #small-triangle").hide();
+	$("#popup-menu ").hide();
 
 	$('#add').click(function(){
-		$("#popup-menu,#small-triangle").toggle(200);
+		$("#popup-menu").toggle(200);
+		
 	})
 	/*  控制顶层弹出菜单层 : end  */
 	
